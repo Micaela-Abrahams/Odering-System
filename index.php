d3a5870b2be551ecc6e17984d243dd26a4999ec1
Micaela-Abrahams <109106058+Micaela-Abrahams@users.noreply.github.com>
2023-02-27 22:47:10 +0200

    layout of index page added

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Stylesheet/style.css">
+    <title>Ordering System:</title>
+</head>
+
+<body>
+    <img src="/images/dropping donuts.png" alt="logo">
+    <form action="checkout.php" method="post">
+
+        <label for="firstName">First Name:</label>
+        <input type="text" name="firstName" id="firstName" required placeholder="First Name">
+
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="lastName" id="lastName" required placeholder="Last Name"><br>
+
+        <label for="emailAddress">Email:</label>
+        <input type="email" name="emailAddress" id="emailAddress" required placeholder="david@gmail.com"><br>
+
+        <label for="contactNumber">Contact:</label>
+        <input type="tel" name="contactNumber" id="contactNumber" required placeholder="071 123 4567">
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <input type="submit" name="checkout" id="checkout" value=checkout>
+
+    </form>
+
+    </form>
+
+</body>
+
+</html>
